19f775a367d4ad19936587ee2a9d556f3c4d2764
Jatuphon Liwakorn <52460048+hiangsui@users.noreply.github.com>
2020-10-30 11:46:46 +0700

    Initial commit

---
+++ Assets/Script/TakePhotoNativePlugin.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+
+public class TakePhotoNativePlugin : MonoBehaviour
+{
+    // Comment : เป็น Plugin สำหรับ Save รูปภาพ //
+
+    public GameObject secondPanel;
+    public Text textDebug;
+    public Text textDebugGet;
+
+    public void ButtonCaptureScreenShot()
+    {
+        StartCoroutine("TakeScreenShot");
+    }
+
+    IEnumerator TakeScreenShot()
+    {
+        string albumName = "YakGameImage";
+        string timeStamp = DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+
+        secondPanel.SetActive(false);
+
+        yield return new WaitForEndOfFrame();
+        Texture2D tex = new Texture2D(Screen.width, Screen.height);
+        tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        tex.Apply();
+
+        yield return tex;
+
+        secondPanel.SetActive(true);
+
+        // Save Image to Gallery //
+
+        NativeGallery.SaveImageToGallery(tex, albumName, timeStamp + ".png", (success, path) => textDebug.text = "Media save result: " + success + " " + path);
+
+        // To avoid memory leaks //
+
+        Destroy(tex);
+    }
+
+    public void ButtonGetImage()
+    {
+        // ลอง Get แบบอันเดียวก่อน //
+
+        string filePath;
+
+        NativeGallery.GetImageFromGallery((path) =>
+        {
+            filePath = path;
+
+            textDebugGet.text = "ImagePath" + path;
+        }, "Select a PNG Image", "image/png");
+
+        //NativeGallery.LoadImageAtPath((filePath));
+    }
+}
